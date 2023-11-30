6f31f674e8b06f304404a2e3916a6117aee15dd9
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2023-11-30 11:49:09 +0300

    Adding title and date implemented

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
 
-    invoice_number = Path(filepath).stem.split("-")[0]
+    invoice_number, date = Path(filepath).stem.split("-")
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_number}")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_number}", ln=1)
+
+    pdf.set_font(family="Times", size=14)
+    pdf.cell(w=50, h=8, txt=f"Date {date}")
+
     pdf.output(f"PDFs/{Path(filepath).stem}.pdf")
